c45ad283ae38390ba6c329c183d688d406f105d3
Yuan Yao <yuan.yao@it.uu.se>
2024-10-16 23:27:10 +0200

    update

--- plot.py
+++ plot.py
@@ -2,6 +2,9 @@ import os
 import matplotlib.pyplot as plt
 import hashlib
 from matplotlib.backends.backend_pdf import PdfPages
+import json
+import numpy as np
+from colorama import Fore, Back, Style
 plt.rc('font', size=10)
 
 
@@ -52,9 +55,10 @@ class wow_color:
 
 class hiplot:
 
-    def __init__(self, SN):
+    def __init__(self, SN=None, color=None):
         self._wc = wow_color()
         self.SN = SN
+        self.color = color
 
     def add_line(self, ax, xpos, ypos, length, style="bar"):
         line = plt.Line2D([xpos, xpos], [ypos + length, ypos],
@@ -85,13 +89,21 @@ class hiplot:
     def label_group_bar(self,
                         ax,
                         data,
-                        xtickon,
+                        bar = 0.3,
+                        xtickon = True,
+                        xinnertickon = True,
                         stack = False,
+                        edgecolor = None,
                         style = "bar",
                         iname="",
-                        legendon=False):
+                        legendon=False,
+                        legendloc=None,
+                        gridlinestyle='-'):
         ax.yaxis.grid(True)
-        ax.yaxis.grid(color='black', linestyle='dotted', zorder=0)
+        ax.yaxis.grid(color='lightgrey',
+                      linestyle=gridlinestyle,
+                      zorder=0,
+                      linewidth=.5)
 
         if stack == False:
             groups = self.mk_groups(data)
@@ -101,9 +113,22 @@ class hiplot:
             xticks = range(1, ly + 1)
 
             if style == "bar":
+                if self.color is None:
+                    cmap = [self._wc(item) for item in x]
+                else:
+                    cmap = []
+                    for e in x:
+                        # TODO what if _ is a string
+                        if e == "seperator":
+                            cmap.append('white')
+                            continue
+                        cmap.append(self.color[int(e) % len(self.color)])
+
                 ax.bar(xticks, y, align='center',
-                       color=[self._wc(item) for item in x],
-                       edgecolor='black', zorder=3)
+                       color = cmap,
+                       edgecolor=edgecolor,
+                       zorder=3)
+
             elif style == "line":
                 ax.plot(xticks, y, zorder=3,
                         label=iname, color=COLOR[iname],
@@ -111,80 +136,148 @@ class hiplot:
             else:
                 sys.exit()
         else:
-            for i in data.keys():
-                groups = mk_groups(data[i])
+            for i,item in enumerate(list(data.keys())):
+                groups = self.mk_groups(data[item])
                 xy = groups.pop()
                 x, y = zip(*xy)
                 ly = len(y)
-                if i == list(data.keys())[0]:
+                if item == list(data.keys())[0]:
                     bot = [0] * ly
                 xticks = range(1, ly + 1)
-                ax.bar(xticks, y, bottom = bot, label=self.SN[i],
-                       align='center',
-                       color= self._wc(i),
-                       edgecolor='black',
-                       zorder=3)
+
+                if self.color is None:
+                    cmap = self._wc(item)
+                else:
+                    cmap = self.color[i]
+
+                ax.bar(xticks, y, bottom = bot,
+                       label=self.SN[item],
+                       align = 'center',
+                       color = cmap,
+                       edgecolor = edgecolor,
+                       zorder = 3)
                 bot = [x+y for x,y in zip(bot, y)]
 
         if legendon == True:
-            ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.4, 0, 0),
-                      ncol=9, edgecolor='black')
+            handles, labels = ax.get_legend_handles_labels()
+            ax.legend(
+                handles[::-1],
+                labels[::-1],
+                loc=legendloc,
+                # bbox_to_anchor=(0.5, 1.4, 0, 0),
+                ncol=4, #TODO This is ad-hoc by far
+                edgecolor='black')
 
         if xtickon == True:
             ax.set_xticks(xticks)
-            ax.set_xticklabels(x, rotation=90)
             ax.set_xlim(.5, ly + .5)
+            if xinnertickon:
+                ax.set_xticklabels(x, rotation=90)
+                # first level x-ticklabel position
+                ypos = -bar
+            else:
+                ax.set_xticklabels([], minor=False)
+                # first level x-ticklabel position
+                ypos = -bar*0.7
 
             scale = 1. / ly
-            for pos in range(ly + 1):  # change xrange to range for python3
-                self.add_line(ax, pos * scale, -0.05, 0.05)
-            ypos = -.6
+            # for pos in range(ly + 1):
+            #     # change xrange to range for python3
+            #     self.add_line(ax, pos * scale, -0.05, 0.05)
             while groups:
                 group = groups.pop()
                 pos = 0
                 for label, rpos in group:
-                    for sn in self.SN.keys():
-                        if sn in label:
-                            label = label.replace(sn, self.SN[sn])
-                            break
+                    if self.SN != None:
+                        for sn in self.SN.keys():
+                            if sn in label:
+                                label = label.replace(sn, self.SN[sn])
+                                break
                     lxpos = (pos + .5 * rpos) * scale
                     ax.text(lxpos, ypos, label, ha='center',
                             transform=ax.transAxes)
-                    self.add_line(ax, pos * scale, -0.6, 0.6)
+                    self.add_line(ax, pos * scale, -bar*0.8, bar*0.8)
                     pos += rpos
-                self.add_line(ax, pos * scale, ypos, 0.6)
-                ypos -= .1
+                self.add_line(ax, pos * scale, ypos, bar)
+                ypos -= .2 # space between cat label and sub-cat label
         else:
             ax.set_xticks(xticks)
             ax.set_xticklabels([])
             ax.set_xlim(.5, ly + .5)
+
+    @classmethod
+    def dictDump(self, _dict, header=" ", percent=False):
+
+        def to_percentage(nested_dict):
+            for key, value in nested_dict.items():
+                if isinstance(value, dict):
+                    # Recursively call the function if the value is another dictionary
+                    to_percentage(value)
+                else:
+                    # Convert the value to a percentage (assuming it's a number)
+                    nested_dict[key] = f"{value * 100:.2f}%"
+            return nested_dict
+
+        if percent:
+            _dict = to_percentage(_dict)
+
+        print(Fore.GREEN + "======" + header + "======" + Style.RESET_ALL)
+        print(json.dumps(
+            _dict,
+            sort_keys=False,
+            indent=4,
+            separators=(',', ': ')
+            )
+        )
 
     def do_plot1(self,
                  dir_name, name, y_title,
                  data,
                  percentage = False,
                  stack = False,
+                 xtickon = True,
+                 xinnertickon = True,
+                 edgecolor = None,
+                 legendon = False,
+                 legendloc = None,
                  width = 30,
+                 height = 2,
+                 bar = 0.3,
                  start = 0,
                  end = 1.0,
-                 step = 0.1):
+                 step = 0.1,
+                 ytitlesize = 12,
+                 yticksize = 12,
+                 gridlinestyle = '--'):
         isExist = os.path.exists(dir_name)
         if not isExist:
             os.makedirs(dir_name)
 
         print(bcolors.GREEN + dir_name + name + '.pdf' + bcolors.ENDC)
-        fig = plt.figure(figsize=(width, 2))
+        fig = plt.figure(figsize=(width, height))
         pp = PdfPages(dir_name + '/' + name + '.pdf')
         ax = fig.add_subplot(1,1,1)
-        self.label_group_bar(ax, data, True, stack)
+        self.label_group_bar(ax,
+                             data,
+                             bar,
+                             xtickon,
+                             xinnertickon,
+                             stack,
+                             edgecolor=edgecolor,
+                             legendon=legendon,
+                             legendloc=legendloc,
+                             gridlinestyle=gridlinestyle)
         fig.subplots_adjust(bottom=0.3)
-        ax.set_ylabel(y_title)
+        ax.set_ylabel(y_title, fontsize=ytitlesize)
+        if (xinnertickon == False) or (xtickon == False):
+            ax.xaxis.set_ticks_position('none')
 
         if percentage is True:
             ax.set_ylim([start, end*1.1])
             ax.set_yticks(np.arange(start, end*1.1, step))
-            ax.set_yticklabels(str(int(x*100)) + '%' \
-                               for x in np.arange(start, end*1.1, step))
+            ax.set_yticklabels([str(int(x*100)) + '%' \
+                               for x in np.arange(start, end*1.1, step)],
+                               fontsize=yticksize)
 
         plt.savefig(pp, format='pdf', bbox_inches='tight')
         pp.close()
